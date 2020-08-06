34c05a603d7a723857bf3dbbd8c2fa95dd06e01e
Jarek Radosz <jradosz@gmail.com>
2020-08-06 12:27:54 +0200

    FIX: Enable the usage of quotes inside bbcode attributes
    Examples:
    
    ```
    [poll title="What's up"][/poll]
    [quote='“the discourse”']
    ```

--- test/javascripts/lib/bbcode-test.js
+++ test/javascripts/lib/bbcode-test.js
@@ -3,9 +3,29 @@ import { parseBBCodeTag } from "pretty-text/engines/discourse-markdown/bbcode-bl
 QUnit.module("lib:pretty-text:bbcode");
 
 QUnit.test("block with multiple quoted attributes", assert => {
-  const parsed = parseBBCodeTag('[test one="foo" two="bar bar"]', 0, 30);
+  const parsed = parseBBCodeTag(`[test one="foo" two='bar bar']`, 0, 30);
 
   assert.equal(parsed.tag, "test");
   assert.equal(parsed.attrs.one, "foo");
   assert.equal(parsed.attrs.two, "bar bar");
+});
+
+QUnit.test("default attribute value", assert => {
+  const parsed = parseBBCodeTag("[test=foobar]", 0, 13);
+
+  assert.equal(parsed.tag, "test");
+  assert.equal(parsed.attrs._default, "foobar");
+});
+
+QUnit.test("allows quote characters inside a another quotes", assert => {
+  const parsed = parseBBCodeTag(
+    `[test one="foo's" two='“bar”' three=“"abc's"”]`,
+    0,
+    46
+  );
+
+  assert.equal(parsed.tag, "test");
+  assert.equal(parsed.attrs.one, "foo's");
+  assert.equal(parsed.attrs.two, "“bar”");
+  assert.equal(parsed.attrs.three, `"abc's"`);
 });
